714e5103f7e024fc35d6105f9a51db195870c005
allaeia <marie.guenon@laposte.net>
2013-10-23 12:25:45 +0200

    tirage

---
+++ js/tirage.js
@@ -0,0 +1,26 @@
+var alphabet = ["B","C","D","F","G","H","J","K","L","M","O","P","Q","R","U","V","W","X","Y","Z","A","E","I","N","S","T"];
+
+function uniforme(){
+	var nombre = Math.floor(Math.random() * 26);
+	return alphabet[nombre];
+}
+
+function non_uniforme(){
+	var nombre = Math.floor(Math.random() * 100)+1;
+	//document.write(nombre+"<br />");
+	if(nombre <= 17.26){
+		return alphabet[21];
+	}else if(nombre <= (17.26+8.40)){
+		return alphabet[20];
+	}else if(nombre <= (17.26+8.40+7.34)){
+		return alphabet[22];
+	}else if(nombre <= (17.26+8.40+7.34+7.13)){
+		return alphabet[23];
+	}else if(nombre <= (17.26+8.40+7.34+7.13+8.08)){
+		return alphabet[24];
+	}else if(nombre <= (17.26+8.40+7.34+7.13+8.08+7.07)){
+		return alphabet[25];
+	}
+	nombre = Math.floor(Math.random() * 20)+1;
+	return alphabet[nombre];
+}
