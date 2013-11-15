3a5f252b144a03b3d3d5a3a37cf96e020f5e8be8
allaeia <marie.guenon@laposte.net>
2013-11-15 22:42:39 +0100

    sd

--- js/js.js
+++ js/js.js
@@ -151,25 +151,13 @@ console.log(x);
 		//on fait rotater le soleil
 		var w = parseInt($("#soleil").attr("width").split("%")[0])/100;
 		var h = parseInt($("#soleil").attr("height").split("%")[0])/100;
-		//var x = parseInt($("#soleil").attr('x').split("px")[0]);
-		//var y = parseInt($("#soleil").attr('y').split("px")[0]);
-		//$('#soleil').css('transform-origin', '50% 50%');
-		//$('#soleil').css('transform', 'translate('+(-w/2)+','+(-h/2)+')');
-		//$('#soleil').css('transform', 'rotate(10deg)');
-		//$('#soleil').css('transform', 'translate('+(w/2)+','+(h/2)+')');
-		//plus d'erruer mais disparait...
-		$("#soleil").attr("transform", "rotate(60 "+(w/2)+" "+(h/2)+")");
-		//var rotation = function (){
-		/*   $("#soleil").rotate({
-		      angle:0, 
-		      animateTo:360, 
-		      callback: rotation,
-		      easing: function (x,t,b,c,d){
-			  return c*(t/d)+b;
-		      }
-		   });
+		for (var i=0; i<5; i++)
+		{
+			//pas la bonne équation !
+			$("#soleil").attr('x',(w/2)+"px");
+			$("#soleil").attr('y',(h/2)+"px");
+			$("#soleil").attr("transform", "rotate(60)");
 		}
-		rotation();*/
 	}
 	else
 	{
